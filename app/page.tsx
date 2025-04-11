8077c5d45b3e40e23d2fa34107151eb58be2c66f
MrSidSir <irshad1554@gmail.com>
2025-04-11 20:01:43 +0530

    i create a concept coaching browser

---
+++ app/page.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div> 
+    <div className=" flex-1/3 m-20 text-center text-red-500 text-shadow-amber-200 text-[50px]" >
+       <h1>  welcome to concept coaching browser </h1>
+   <h2>about our browser</h2>
+   <p>
+this is  an international browser
+   </p>
+
+   
+   
+       </div>
+
+
+
+       <div>
+        h1
+       </div>
+  </div>
+  );
+}
